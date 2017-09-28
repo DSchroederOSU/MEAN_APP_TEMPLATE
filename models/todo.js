bcff18c1d2f1b43d211b11c990c0d4288dac3bca
DSchroederOSU <schrodan@oregonstate.edu>
2017-09-28 12:12:43 -0700

    mongo add

---
+++ models/todo.js
@@ -0,0 +1,9 @@
+//models/todo.js
+
+// load mongoose since we need it to define a model
+var mongoose = require('mongoose');
+
+module.exports = mongoose.model('Todo', {
+    text : String,
+    done : Boolean
+});
